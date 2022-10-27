1718447d58cb60ae67fd09d5787541d785083484
David Morgan <30870056+haitheredavid@users.noreply.github.com>
2022-10-27 14:03:33 -0400

    refactored gizmo component

--- Assets/dscripts/Pieces/GizmoComponent.cs
+++ Assets/dscripts/Pieces/GizmoComponent.cs
@@ -1,13 +1,27 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 namespace HaiThere.Playbook
 {
-  public abstract class GizmoComponent : MonoBehaviour
-  {
-    [SerializeField] protected GameObject obj;
 
-    public Vector3 pointerPos { get; protected set; }
+  public abstract class GizmoComponent : Gizmo
+  {
+    [Header("Component Props")]
+    public Camera viewer;
+    public bool isParented = false;
+    public bool isLocal = true;
+    [SerializeField] protected GameObject obj;
+    [Range(1f, 100f)] public float movementSpeed = 100f;
+    [SerializeField, Range(0.0f, 1.0f)] protected float positionOffset = 0.5f;
+    [SerializeField, Range(0.0f, 1.0f)] protected float scaleOffset = 0.5f;
+    [SerializeField] protected Vector3 anchorOffset = new Vector3(0.2f, 0.0f, 0.2f);
+
+    protected List<GizmoPiece> pieces = new List<GizmoPiece>();
+
+
+    public GizmoPiece activePiece { get; protected set; }
 
     public GameObject activeObj
     {
@@ -22,15 +36,95 @@ namespace HaiThere.Playbook
 
         foreach (var piece in pieces)
         {
-          piece.focusObj = value.transform;
+          piece.obj = value.transform;
         }
       }
     }
 
-    protected List<GizmoPiece> pieces = new List<GizmoPiece>();
+    public Vector3 pointerPos
+    {
+      get => viewer == null ? Vector3.zero : viewer.ScreenToWorldPoint(Input.mousePosition);
+    }
+    
+    public event UnityAction OnActionComplete;
 
+    public override void SetActive(bool status)
+    {
+      foreach (var p in pieces)
+      {
+        p.SetActive(status);
+      }
+    }
 
-    public abstract void Create();
+    protected void SetActivePiece(GizmoPiece piece)
+    {
+      if (piece == null)
+        return;
+
+      if (activePiece != null)
+      {
+        if (activePiece.Equals(piece))
+          return;
+
+        activePiece.OnComplete -= CompleteActionHook;
+      }
+
+      activePiece = piece;
+      activePiece.OnComplete += CompleteActionHook;
+    }
+    
+    void CompleteActionHook()
+    {
+      activePiece.transform.localPosition = GetPiecePosition(activePiece.axis);
+      OnActionComplete?.Invoke();
+
+      if (!isParented)
+        MoveToObjectAnchor();
+    }
+
+    protected virtual void MoveToObjectAnchor()
+    {
+      transform.position = new Vector3(
+        activeObj.transform.position.x - activeObj.transform.localScale.x * 0.5f + anchorOffset.x,
+        activeObj.transform.position.y - activeObj.transform.localScale.y * 0.5f + anchorOffset.y,
+        activeObj.transform.position.z - activeObj.transform.localScale.z * 0.5f + anchorOffset.z
+      );
+    }
+
+    protected virtual Vector3 GetPiecePosition(AxisType axis)
+    {
+      var offset = Mathf.Max(positionOffset, scaleOffset * 0.5f);
+      return axis switch
+      {
+        AxisType.X => new Vector3(offset, 0, 0),
+        AxisType.Y => new Vector3(0, offset, 0),
+        AxisType.Z => new Vector3(0, 0, offset),
+        _ => Vector3.zero
+      };
+    }
+
+    protected virtual Vector3 GetPieceRotation(AxisType axis)
+    {
+      return axis switch
+      {
+        AxisType.X => new Vector3(0, -90, -90),
+        AxisType.Y => new Vector3(90, 0, 0),
+        AxisType.Z => new Vector3(0, 0, -90),
+        _ => Vector3.zero
+      };
+    }
+    
+    protected virtual TPiece BuildPrefab<TPiece>() where TPiece : GizmoPiece
+    {
+      var prefab = new GameObject().AddComponent<TPiece>();
+
+      prefab.GetComponent<MeshFilter>().mesh = PlaybookMeshBuilder.CreatCube(1f);
+      prefab.GetComponent<MeshCollider>().sharedMesh = prefab.GetComponent<MeshFilter>().mesh;
+      prefab.GetComponent<MeshRenderer>().material = Resources.Load<Material>("Gizmo");
+      prefab.movementSpeed = movementSpeed;
+      prefab.isLocal = isLocal;
+      return prefab;
+    }
 
   }
 }
--- Assets/dscripts/Pieces/GizmoComponentMover.cs
+++ Assets/dscripts/Pieces/GizmoComponentMover.cs
@@ -1,5 +1,4 @@
 using System;
-using System.Collections.Generic;
 using HaiThere.Playbook;
 using UnityEngine;
 using UnityEngine.Events;
@@ -7,36 +6,16 @@ using UnityEngine.Events;
 
 public class GizmoComponentMover : GizmoComponent
 {
-  [Header("Gizmo Props")]
-  public Camera viewer;
-  public bool isParented;
-  [Range(1f, 100f)] public float movementSpeed = 100f;
-
   [Header("Handler Props")]
-  [SerializeField, Range(0.0f, 1.0f)] float positionOffset = 0.5f;
-  [SerializeField, Range(0.0f, 1.0f)] float scaleOffset = 0.5f;
-  [SerializeField] Vector3 anchorOffset = new Vector3(0.2f, 0.0f, 0.2f);
   [SerializeField] bool showLine = true;
 
   [Header("Debug Position")]
-  [SerializeField] bool useLocalPos = true;
   [SerializeField] bool logPos = false;
-
-  public GizmoPieceMove activePiece { get; private set; }
-
-  public event UnityAction OnActionComplete;
-
+  
   public override void Create()
   {
-    var prefab = new GameObject().AddComponent<GizmoPieceMove>();
-
-    prefab.GetComponent<MeshFilter>().mesh = PlaybookMeshBuilder.CreatCube(1f);
-    prefab.GetComponent<MeshCollider>().sharedMesh = prefab.GetComponent<MeshFilter>().mesh;
-    prefab.GetComponent<MeshRenderer>().material = Resources.Load<Material>("Gizmo");
-
+    var prefab = BuildPrefab<GizmoPieceMove>();
     prefab.showLine = showLine;
-    prefab.isLocal = useLocalPos;
-    prefab.movementSpeed = movementSpeed;
 
     // hard coded size since its just using a cube but this should change for a model later
     prefab.transform.localScale = new Vector3(.2f, .2f, .5f);
@@ -50,84 +29,15 @@ public class GizmoComponentMover : GizmoComponent
       instance.axis = axis;
       instance.parent = this;
 
-      instance.OnClick += SetActivePiece;
-
+      instance.OnSet += SetActivePiece;
       instance.Create();
 
       pieces.Add(instance);
     }
-
+    
     Destroy(prefab.gameObject);
   }
 
 
-  void SetActivePiece(GizmoPieceMove piece)
-  {
-    if (piece == null)
-      return;
-
-    if (activePiece != null)
-    {
-      if (activePiece.Equals(piece))
-        return;
-
-      activePiece.OnStart -= StartActionHook;
-      activePiece.OnEnd -= EndActionHook;
-    }
-
-    activePiece = piece;
-    activePiece.OnStart += StartActionHook;
-    activePiece.OnEnd += EndActionHook;
-  }
-
-  void StartActionHook()
-  {
-  }
-
-  void EndActionHook()
-  {
-    activePiece.transform.localPosition = GetPiecePosition(activePiece.axis);
-    OnActionComplete?.Invoke();
-
-    if (!isParented)
-      MoveToObjectAnchor();
-  }
-
-  public Vector3 pointerPos
-  {
-    get => viewer == null ? Vector3.zero : viewer.ScreenToWorldPoint(Input.mousePosition);
-  }
-
-  void MoveToObjectAnchor()
-  {
-    transform.position = new Vector3(
-      activeObj.transform.position.x - activeObj.transform.localScale.x * 0.5f + anchorOffset.x,
-      activeObj.transform.position.y - activeObj.transform.localScale.y * 0.5f + anchorOffset.y,
-      activeObj.transform.position.z - activeObj.transform.localScale.z * 0.5f + anchorOffset.z
-    );
-  }
-
-  Vector3 GetPiecePosition(AxisType axis)
-  {
-    var offset = Mathf.Max(positionOffset, scaleOffset * 0.5f);
-    return axis switch
-    {
-      AxisType.X => new Vector3(offset, 0, 0),
-      AxisType.Y => new Vector3(0, offset, 0),
-      AxisType.Z => new Vector3(0, 0, offset),
-      _ => Vector3.zero
-    };
-  }
-
-  Vector3 GetPieceRotation(AxisType axis)
-  {
-    return axis switch
-    {
-      AxisType.X => new Vector3(0, -90, -90),
-      AxisType.Y => new Vector3(90, 0, 0),
-      AxisType.Z => new Vector3(0, 0, -90),
-      _ => Vector3.zero
-    };
-  }
 
 }

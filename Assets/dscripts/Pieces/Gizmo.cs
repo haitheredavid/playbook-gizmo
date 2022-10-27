a068e9dfbb7d507c039bf962b3a1d676fec71e44
David Morgan <30870056+haitheredavid@users.noreply.github.com>
2022-10-27 14:03:15 -0400

    created gizmo top class

---
+++ Assets/dscripts/Pieces/Gizmo.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+namespace HaiThere.Playbook
+{
+  public abstract class Gizmo : MonoBehaviour
+  {
+    public abstract void Create();
+    public abstract void SetActive(bool status);
+
+  }
+}

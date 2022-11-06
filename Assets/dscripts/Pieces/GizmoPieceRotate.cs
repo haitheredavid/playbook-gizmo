d1e987e218918c0dfeffc73e97b95918476c2dda
David Morgan <30870056+haitheredavid@users.noreply.github.com>
2022-11-06 17:45:27 -0500

    tackling gizmo piece rotater

--- Assets/dscripts/Pieces/GizmoPieceRotate.cs
+++ Assets/dscripts/Pieces/GizmoPieceRotate.cs
@@ -7,24 +7,9 @@ namespace HaiThere.Playbook
   public class GizmoPieceRotate : GizmoPiece
   {
 
-    public int sides { get; set; } = 50;
-    public float offsetSize{ get; set; }  = 1f;
-    public float innerRadius { get; set; } = 2f;
-
-    Vector3 prevAngle;
-    Vector3 objAngle;
-
-
-    public void Start()
+    protected override void ApplyResult(Vector3 result)
     {
-      prevAngle = transform.localRotation.eulerAngles;
-      material.color = PlaybookColors.SetInactive(axis switch
-      {
-        AxisType.X => PlaybookColors.AxisX,
-        AxisType.Y => PlaybookColors.AxisY,
-        AxisType.Z => PlaybookColors.AxisZ,
-        _ => material.color
-      });
+      obj.transform.localRotation = Quaternion.Euler(result);
     }
 
     // public void Create(AxisType axisType)
@@ -42,97 +27,67 @@ namespace HaiThere.Playbook
     //   Create();
     // }
 
-    public override void Create()
-    {
-      base.Create();
-      var mesh = Builder.CreateCircle(meshFilter.transform.localPosition, sides, innerRadius, offsetSize);
-
-      if (Application.isPlaying)
-      {
-        meshFilter.mesh = mesh;
-        meshCollider.sharedMesh = meshFilter.mesh;
-      }
-      else
-      {
-        meshFilter.sharedMesh = mesh;
-        meshCollider.sharedMesh = meshFilter.sharedMesh;
-      }
-    }
-
-    public override void OnBeginDrag(PointerEventData eventData)
-    {
-      material.color = axis switch
-      {
-        AxisType.X => PlaybookColors.AxisX,
-        AxisType.Y => PlaybookColors.AxisY,
-        AxisType.Z => PlaybookColors.AxisZ,
-        _ => material.color
-      };
-
-      prevAngle = transform.localRotation.eulerAngles;
-
-      if (obj != null)
-      {
-        objAngle = obj.transform.localRotation.eulerAngles;
-      }
-    }
-
-    public override void OnDrag(PointerEventData eventData)
-    {
-      var delta = (eventData.delta.x + eventData.delta.y) * precision;
-
-      switch (axis)
-      {
-        case AxisType.X:
-          delta *= -1;
-          prevAngle += new Vector3(0, delta, 0);
-          objAngle = new Vector3(objAngle.x, objAngle.y + delta, objAngle.z);
-          break;
-        case AxisType.Y:
-          prevAngle += new Vector3(0, 0, delta);
-          objAngle = new Vector3(objAngle.x, objAngle.y, objAngle.z + delta);
-          break;
-        case AxisType.Z:
-          delta *= -1;
-          prevAngle += new Vector3(0, 0, delta);
-          objAngle = new Vector3(objAngle.x + delta, objAngle.y, objAngle.z);
-          break;
-      }
-
-      transform.localRotation = Quaternion.Euler(prevAngle.x, prevAngle.y, prevAngle.z);
-
-      if (obj != null)
-      {
-        obj.transform.localRotation = Quaternion.Euler(objAngle);
-      }
-    }
-
-
-    public override void OnEndDrag(PointerEventData eventData)
-    {
-      material.color = PlaybookColors.SetInactive(axis switch
-      {
-        AxisType.X => PlaybookColors.AxisX,
-        AxisType.Y => PlaybookColors.AxisY,
-        AxisType.Z => PlaybookColors.AxisZ,
-        _ => material.color
-      });
-    }
-    protected override void ApplyResult(Vector3 result)
-    {
-    }
-    // protected override void UpdateToNewObject()
+    // public override void OnBeginDrag(PointerEventData eventData)
     // {
-    // 	var size = Obj.objectBounds.size;
+    //   material.color = axis switch
+    //   {
+    //     AxisType.X => PlaybookColors.AxisX,
+    //     AxisType.Y => PlaybookColors.AxisY,
+    //     AxisType.Z => PlaybookColors.AxisZ,
+    //     _ => material.color
+    //   };
     //
-    // 	innerRadius = Mathf.Max(
-    // 		size.x * (Obj.transform.localScale.x * 0.8f),
-    // 		size.y * (Obj.transform.localScale.y * 0.8f),
-    // 		size.z * (Obj.transform.localScale.z * 0.8f)
-    // 	);
+    //   prevAngle = transform.localRotation.eulerAngles;
     //
-    // 	Create();
+    //   if (obj != null)
+    //   {
+    //     objAngle = obj.transform.localRotation.eulerAngles;
+    //   }
     // }
+    //
+    // public override void OnDrag(PointerEventData eventData)
+    // {
+    //   var delta = (eventData.delta.x + eventData.delta.y) * precision;
+    //
+    //   switch (axis)
+    //   {
+    //     case AxisType.X:
+    //       delta *= -1;
+    //       prevAngle += new Vector3(0, delta, 0);
+    //       objAngle = new Vector3(objAngle.x, objAngle.y + delta, objAngle.z);
+    //       break;
+    //     case AxisType.Y:
+    //       prevAngle += new Vector3(0, 0, delta);
+    //       objAngle = new Vector3(objAngle.x, objAngle.y, objAngle.z + delta);
+    //       break;
+    //     case AxisType.Z:
+    //       delta *= -1;
+    //       prevAngle += new Vector3(0, 0, delta);
+    //       objAngle = new Vector3(objAngle.x + delta, objAngle.y, objAngle.z);
+    //       break;
+    //   }
+    //
+    //   transform.localRotation = Quaternion.Euler(prevAngle.x, prevAngle.y, prevAngle.z);
+    //
+    //   if (obj != null)
+    //   {
+    //     obj.transform.localRotation = Quaternion.Euler(objAngle);
+    //   }
+    // }
+    //
+    //
+    // public override void OnEndDrag(PointerEventData eventData)
+    // {
+    //   material.color = PlaybookColors.SetInactive(axis switch
+    //   {
+    //     AxisType.X => PlaybookColors.AxisX,
+    //     AxisType.Y => PlaybookColors.AxisY,
+    //     AxisType.Z => PlaybookColors.AxisZ,
+    //     _ => material.color
+    //   });
+    // }
+    //
+
 
   }
 }
